cb1c81eedfabd286d4cb847cfe526b08693491db
Vitalii Berko <vitalii.v.berko@gmail.com>
2022-12-20 15:43:47 +0200

    Fixed

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,14 +1,11 @@
 import PropTypes from 'prop-types';
+import clsx from 'clsx';
 import s from 'components/FriendListItem/friendListItem.module.css';
 
 export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <>
-      <span
-        className={s.status}
-        // {statusColor={isOnline}}
-        style={{ backgroundColor: isOnline ? 'green' : 'red' }}
-      ></span>
+      <span className={clsx(s.status, isOnline && s.active)}></span>
       <img className={s.avatar} src={avatar} alt="User avatar" width="48" />
       <p className={s.name}>{name}</p>
     </>
